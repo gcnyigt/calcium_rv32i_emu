24a195e5b64f0f712a9f93ba04cedc0a83545606
gcn_yigt_ <selahattinyigitgucin@gmail.com>
2026-05-19 16:15:08 +0300

    Update build_program.sh

--- build_program.sh
+++ build_program.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Building bootrom"
+echo "Building program"
 riscv64-linux-gnu-as -march=rv32i_zicsr -mabi=ilp32 program.s -o program.o
 riscv64-linux-gnu-ld -m elf32lriscv -Ttext=0x8 program.o -o program.elf
 echo "Generating hex code"
